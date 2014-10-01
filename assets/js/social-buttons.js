92f00fe2336f3c4347964af07072b3be4d1c3959
Joe Vasquez <joseph.c.vasquez@gmail.com>
2014-09-30 21:57:22 -0700

    Main commit

---
+++ assets/js/social-buttons.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	  $('.facebook').click(function(){
+		window.open(
+		  'https://www.facebook.com/sharer/sharer.php?u=http://www.20mmscholarship.org', 'popUpWindow','height=700,width=800,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes'
+		);
+	  });
+	  $('.youtube').click(function(){
+		window.open(
+		  'https://plus.google.com/share?url=http://www.20mmscholarship.org', 'popUpWindow','height=700,width=800,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes'
+		);
+	  });	  
+	  $('.twitter').click(function(){
+		window.open(
+		  'http://twitter.com/home?status=Got 30 seconds? Enter to win a $2,500 textbook scholarship at 20mmscholarship.org. Thank you @20MillionMinds!+http://www.20mmscholarship.org','popUpWindow','height=700,width=800,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes'
+		);
+	  });
+	  $('.instagram').click(function(){
+		window.open(
+		  'http://instagram.com/20_mm', 'popUpWindow','height=700,width=800,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes'
+		);
+	  });	 	  	  
+	});
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Social Buttons</title>
+	<meta name="author" content="Joe Vasquez" />
+	<meta name="description" content="Tell us how you cover the cost of your textbooks. Make it funny and touching. Enter to win a $2,500 book scholarship from The Michelson 20MM Foundation." />
+	<meta name="keywords"  content="Joe Vasquez, joseph.c.vasquez@gmail.com, social buttons" />
+	<meta name="Resource-type" content="Document" />
+    
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>	
+	<link rel="stylesheet" type="text/css" href="assets/css/social-buttons.css" />
+	<script type="text/javascript" src="assets/js/social-buttons.js"></script>  
+  
+</head>
+<body style="background-color:#000">
+
+
+
+
+
+	<div class="container">
+    	<div class="socialButton facebook">
+        	<div class="socialButtonContent">Share</div>
+        </div>
+    	<div class="socialButton youtube">
+        	<div class="socialButtonContent">Follow</div>
+        </div>
+    	<div class="socialButton twitter">
+        	<div class="socialButtonContent">Tweet</div>
+        </div>
+    	<div class="socialButton instagram">
+        	<div class="socialButtonContent">Snap</div>
+        </div>
+    </div>
+
+
+
+
+</body>
+</html>
